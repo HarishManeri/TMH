06bfd48503e2d32708e1bf40cb1ba8c654bdf5c3
HarishManeri <maneriharish21@gmail.com>
2025-04-26 10:22:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const rescheduleBtn = document.querySelector('.reschedule');
+    const viewDetailsBtn = document.querySelector('.view-details');
+
+    rescheduleBtn.addEventListener('click', () => {
+        alert('Reschedule feature coming soon!');
+    });
+
+    viewDetailsBtn.addEventListener('click', () => {
+        alert('View details feature coming soon!');
+    });
+});
